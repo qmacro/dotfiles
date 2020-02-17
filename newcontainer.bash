b7787d58ff887646295c1fabd9d602687c5a7a72
DJ Adams <dj.adams@pobox.com>
2020-02-17 12:14:21 +0000

    add newcontainer script

---
+++ newcontainer.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+heading () {
+  figlet $@
+}
+
+CONTAINER=${1:-dev}
+USER=${2:-dj}
+
+heading Container "${CONTAINER}"
+
+heading Setup and package update
+lxc launch images:debian/buster ${CONTAINER}
+lxc exec ${CONTAINER} -- 'apt update && apt upgrade -y'
+
+heading Install of basic tools
+lxc exec ${CONTAINER} -- apt install -y git curl tmux ranger hub fzf figlet man lolcat sudo
+
+heading Create user "${USER}" and environment
+lxc exec ${CONTAINER} -- useradd -m -s /bin/bash ${USER}
+lxc exec ${CONTAINER} -- mkdir /home/${USER}/.ssh
+lxc exec ${CONTAINER} -- chown ${USER}:${USER} /home/${USER}/.ssh
+lxc exec ${CONTAINER} -- chmod 700 /home/${USER}/.ssh
+lxc file push ${HOME}/.ssh/id_rsa* ${HOME}/.ssh/known_hosts* ${CONTAINER}/home/${USER}/.ssh/
+
+heading Cloning 'dotfiles' repo
+lxc exec ${CONTAINER} -- su -c "git clone git@github.com:qmacro/dotfiles.git ~/.dotfiles" dj
+
+heading Running dotfiles setup
+lxc exec ${CONTAINER} -- su -c "~/.dotfiles/setup.bash" dj
+
+heading Entering ${CONTAINER} as ${USER}
+echo "(Don't forget to install tmux plugins with 'prefix I')"
+lxc exec ${CONTAINER} -- su - ${USER}
+
+
