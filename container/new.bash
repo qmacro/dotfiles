345ccaba4cbab7bcc692284b7d1085c6acf78152
DJ Adams <dj.adams@pobox.com>
2020-03-02 17:39:40 +0000

    move nvm / Node.js install to setup

--- container/new.bash
+++ container/new.bash
@@ -32,10 +32,6 @@ lxc exec ${CONTAINER} -- su -c "git clone git@github.com:qmacro/dotfiles.git ~/.
 heading Running dotfiles setup
 lxc exec ${CONTAINER} -- su -c "~/.dotfiles/setup.bash" ${USER}
 
-heading Installing Node.js 12 LTS
-
-lxc exec ${CONTAINER} -- su -c ". ~/.nvm/nvm.sh && nvm install 12 lts" ${USER}
-
 heading Entering ${CONTAINER} as ${USER}
 echo "(Don't forget to install tmux plugins with 'prefix I')"
 lxc exec ${CONTAINER} -- su - ${USER}
