8080cc00ef14da6ddef0deffc540f098d6aaebda
DJ Adams <dj.adams@sap.com>
2020-10-18 18:18:58 +0100

    redo prompt

--- bashrc.d/30-prompt.sh
+++ bashrc.d/30-prompt.sh
@@ -1,12 +1,21 @@
-export PS1_START="\[\033[36m\]\u\[\033[33m\]@\[\033[1;34m\]\h\[\033[0m\]"
-export PS1_DIR="\[\033[0;33m\][\w]\[\033[0m\]"
-export PS1_END="\n\[\033[37m\]> \[\033[0m\]"
-PS1="${PS1_DIR} ${PS1_END}"
+#!/bin/bash
 
-# For bash-git-prompt (https://github.com/magicmonty/bash-git-prompt)
-if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-  GIT_PROMPT_ONLY_IN_REPO=1
-  GIT_PROMPT_START="${PS1_DIR}"
-  GIT_PROMPT_END="${PS1_END}"
-  source $HOME/.bash-git-prompt/gitprompt.sh
-fi
+# GIT bash integration
+# See https://gist.github.com/eliotsykes/47516b877f5a4f7cd52f
+
+# shellcheck disable=SC1090
+. "$HOME/.dotfiles/git-prompt.sh"
+
+declare red='\e[31m'
+declare reset='\e[0m'
+
+export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWUPSTREAM="auto"
+
+__errorcolour() {
+  [[ $1 -eq 0 ]] && echo "$reset" || echo "$red"
+}
+
+PROMPT_COMMAND='__git_ps1 "\w" "\n$(__errorcolour $?)> $reset"'
