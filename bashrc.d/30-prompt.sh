ab54508f7a02e4c2c75d84416724a253024a1829
DJ Adams <dj.adams@sap.com>
2020-10-19 18:12:02 +0100

    fix up prompt with err col

--- bashrc.d/30-prompt.sh
+++ bashrc.d/30-prompt.sh
@@ -3,20 +3,33 @@
 # GIT bash integration
 # See https://gist.github.com/eliotsykes/47516b877f5a4f7cd52f
 
+export GIT_PS1_SHOWCOLORHINTS='y'
+export GIT_PS1_SHOWDIRTYSTATE='y'
+export GIT_PS1_SHOWUNTRACKEDFILES='y'
+export GIT_PS1_DESCRIBE_STYLE='contains'
+export GIT_PS1_SHOWUPSTREAM='auto'
+
 # shellcheck disable=SC1090
 . "$HOME/.dotfiles/git-prompt.sh"
 
-declare red='\e[31m'
-declare reset='\e[0m'
-
-export GIT_PS1_SHOWCOLORHINTS=true
-export GIT_PS1_SHOWDIRTYSTATE=true
-export GIT_PS1_SHOWUNTRACKEDFILES=true
-export GIT_PS1_SHOWUPSTREAM="auto"
-
-__errorcolour() {
-  [[ $1 -eq 0 ]] && echo "$reset" || echo "$red"
+__showlocation() {
+  # Don't show work userids
+  local username=$1
+  if [[ ${username,,} =~ ^[idc][0-9]+$ ]]; then
+    echo "\\w"
+  else
+    echo "\$PWD"
+  fi
 }
 
-#PROMPT_COMMAND='__git_ps1 "\w" "\n$(__errorcolour $?)> $reset"'
-PROMPT_COMMAND='__git_ps1 "\w" "\n> "'
+__prompt_command() {
+    local EXIT="$?"
+    PS1=$'\u256d\u2500'"$(__showlocation "$USER")$(__git_ps1)\\n"$'\u2570\u2500'
+    if [ $EXIT != 0 ]; then
+      PS1+=$'\[\e[0;31m\]> \[\e[0m\]'
+    else
+      PS1+=$'> '
+    fi
+}
+
+PROMPT_COMMAND=__prompt_command
