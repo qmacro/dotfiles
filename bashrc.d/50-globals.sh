2c2be65fd1d21160bebf938699646ac63ad2357b
DJ Adams <dj.adams@sap.com>
2020-10-19 07:59:08 +0100

    tweak tmux settings

--- bashrc.d/50-globals.sh
+++ bashrc.d/50-globals.sh
@@ -4,3 +4,5 @@ export SAPCP_CLIENTCONFIG=${HOME}/.config/sapcp
 # ln -s -f "$HOME/.dotfiles/host.aliases" "$HOME/.config/"
 export HOSTALIASES="$HOME/.config/host.aliases"
 export BLOG_REPO_LOCATION=~/local/projects
+
+export TERM=xterm-256color
