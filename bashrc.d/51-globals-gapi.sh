6e501dab05bd935d2b6687a93ead31153906bd2d
DJ Adams <dj.adams@sap.com>
2020-11-02 14:38:55 +0000

    add config for GAPI

---
+++ bashrc.d/51-globals-gapi.sh
@@ -0,0 +1,9 @@
+# For general Google API use
+export GAPI_ENDPOINT_AUTH=https://accounts.google.com/o/oauth2/v2/auth
+export GAPI_ENDPOINT_TOKEN=https://www.googleapis.com/oauth2/v3/token
+export GAPI_ENDPOINT_TOKENINFO=https://www.googleapis.com/oauth2/v3/tokeninfo
+export CALLBACK_OOB_URI=urn:ietf:wg:oauth:2.0:oob
+
+# For YouTube streaming API use
+export SCOPE_RW_YT=https://www.googleapis.com/auth/youtube
+export OAUTH_CONTEXT=yt
