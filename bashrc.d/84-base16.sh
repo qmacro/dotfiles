6d8bf59a496ee37324618df8ec23ee4a6b3d4967
DJ Adams <dj.adams@sap.com>
2020-11-18 11:14:28 +0000

    add base16 scheme helper

--- bashrc.d/84-base16.sh
+++ bashrc.d/84-base16.sh
@@ -3,3 +3,24 @@
 # Requires the repo to be cloned into ~/.config/base16-shell
 BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && [ -s "$BASE16_SHELL/profile_helper.sh" ] && eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+# Add helper script to choose from favs
+bscheme() {
+  local choice
+  choice=$(cat <<EOF | fzf
+default-dark
+circus
+gruvbox-dark-soft
+eighties
+woodland
+seti
+railscasts
+phd
+oceanictext
+monokai
+materia
+EOF
+  )
+  _base16 "$BASE16_SHELL/scripts/base16-${choice}.sh"
+}
+
