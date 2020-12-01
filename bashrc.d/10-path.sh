4025a1ae0ec5933279c06aab7e2fb2074009f3a6
DJ Adams <dj.adams@sap.com>
2020-12-01 11:15:49 +0000

    remove nvm, use brew node@12 instead (The nvm setup at shell startup was getting ridiculously long)

--- bashrc.d/10-path.sh
+++ bashrc.d/10-path.sh
@@ -3,3 +3,6 @@ export PATH=/usr/local/opt/openjdk/bin:$PATH # brew-installed Java
 
 # Temp/specific projects as WIP
 export PATH=$PATH:$HOME/Projects/untappd-shell
+
+# Brew-installed Node.js v12
+export PATH="/usr/local/opt/node@12/bin:$PATH"
--- bashrc.d/81-nvm.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck disable=SC1090
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
