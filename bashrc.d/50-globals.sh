a041133ecbdd0d6fc7789488718bc5ac04222fa4
DJ Adams <dj.adams@sap.com>
2021-02-01 07:56:22 +0000

    improve newpost script

--- bashrc.d/50-globals.sh
+++ bashrc.d/50-globals.sh
@@ -3,7 +3,7 @@ export SAPCP_CLIENTCONFIG=${HOME}/.config/sapcp
 
 # ln -s -f "$HOME/.dotfiles/host.aliases" "$HOME/.config/"
 export HOSTALIASES="$HOME/.config/host.aliases"
-export BLOG_REPO_LOCATION=~/local/projects
+export BLOG_REPO_LOCATION="$HOME/Projects"
 
 export TERM=xterm-256color
 
