f02270c531db5479351cae39f37038e1b089c2c4
DJ Adams <dj.adams@sap.com>
2021-02-02 11:03:51 +0000

    improve new blog post workflow

--- bashrc.d/70-functions.sh
+++ bashrc.d/70-functions.sh
@@ -1,3 +1,19 @@
 repo() {
   export REPO="$1"
 }
+
+newpost() {
+
+  local blog
+  blog=$(
+    printf "qmacro.github.io\nautodidactics" \
+      | fzf --no-border --inline-info
+    )
+
+  if [[ -n "$blog" ]]; then
+    cd "$HOME/Projects/$blog" || exit 1
+    createnewpost "$@"
+    git status
+  fi
+
+}
