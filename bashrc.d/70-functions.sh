64423edd0bd8cc3e6cffbcea004001566b681176
DJ Adams <dj.adams@sap.com>
2021-03-12 06:43:51 +0000

    work on blog post functions

--- bashrc.d/70-functions.sh
+++ bashrc.d/70-functions.sh
@@ -2,19 +2,49 @@ repo() {
   export REPO="$1"
 }
 
+chooseblog() {
+  printf "qmacro.github.io\nautodidactics" \
+    | fzf --no-border --inline-info
+}
+
 newpost() {
 
-  local blog location
-  blog=$(
-    printf "qmacro.github.io\nautodidactics" \
-      | fzf --no-border --inline-info
-    )
+  if [[ $# -eq 0 ]]; then
+
+    echo "No title supplied"
+
+  else
+
+    local blog location
+    blog=$(chooseblog)
+
+    if [[ -n "$blog" ]]; then
+      location="$HOME/Projects/$blog"
+      cd "$location" || exit 1
+
+      tmux new -s "$blog"
+      vim +7 -c "Goyo" "$location/$(createnewpost "$@")"
+      git status
+    fi
+
+  fi
+
+}
+
+editpost() {
+
+  local blog location post
+  blog=$(chooseblog)
 
   if [[ -n "$blog" ]]; then
     location="$HOME/Projects/$blog"
     cd "$location" || exit 1
-    vim +7 -c "Goyo" "$location/$(createnewpost "$@")"
-    git status
+
+    # shellcheck disable=2012
+    post=$(ls -1tr _posts/ | tail -1)
+
+    tmux new -s "$blog" "TERM=xterm-256color vim +7 -c 'Goyo' $location/_posts/$post"
   fi
 
 }
+
