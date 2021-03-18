6062c0c47ade6de1b94cd5b80a7bcded4dfa2edb
DJ Adams <dj.adams@sap.com>
2021-03-18 08:20:13 +0000

    uppercase earlier

--- scripts/tlib.sh
+++ scripts/tlib.sh
@@ -6,11 +6,11 @@ setup() {
   # Take any given context, e.g. "yt" and prepare it for use
   # as a suffix to env var names (prefix with _ and uppercasing it).
   local suffixval="${1:-$OAUTH_CONTEXT}"
-  export suffix="${suffixval:+"_$suffixval"}"
+  export suffix="${suffixval:+"_${suffixval^^}"}"
 
   # These vars can be dynamic (with or without suffix)
-  export client_id_var="CLIENT_ID${suffix^^}"
-  export client_secret_var="CLIENT_SECRET${suffix^^}"
+  export client_id_var="CLIENT_ID${suffix}"
+  export client_secret_var="CLIENT_SECRET${suffix}"
 
   # Abort if there's no value for the client ID
   if [[ -z "${!client_id_var}" ]]; then
