e6b37feabd8f72e4cff3f5d138dd97d78e8935da
DJ Adams <dj.adams@sap.com>
2021-07-09 13:56:37 +0100

    add go and gcloud

---
+++ bashrc.d/86-gcloud.sh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/i347491/google-cloud-sdk/path.bash.inc' ]; then . '/Users/i347491/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/i347491/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/i347491/google-cloud-sdk/completion.bash.inc'; fi
