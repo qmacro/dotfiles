c24b18b00f14922b8aa1b039c56b7f6f329a1bd5
DJ Adams <dj.adams@sap.com>
2022-06-30 08:47:49 +0100

    have my personal bin take prio

--- bashrc.d/10-path.sh
+++ bashrc.d/10-path.sh
@@ -1,5 +1,4 @@
-export PATH=$PATH:$HOME/bin
-export PATH=/usr/local/opt/openjdk/bin:$PATH # brew-installed Java
+export PATH="/usr/local/opt/openjdk/bin:$PATH" # brew-installed Java
 
 # Brew-installed Node.js v12
 export PATH="/usr/local/opt/node@12/bin:$PATH"
@@ -10,5 +9,8 @@ export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 
 # Add dotfiles scripts path and subpaths
 while read -r path; do
-  export PATH=$PATH:$path
+  export PATH="$PATH:$path"
 done <<< "$(find "$DOTFILES/scripts" -type d)"
+
+# Have my user-specific bin dir take priority
+export PATH="$HOME/bin:$PATH"
