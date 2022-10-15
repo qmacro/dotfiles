ba21bed8c3a1a8191bbee3a37f427eda1984cf75
DJ Adams <dj.adams@sap.com>
2022-10-15 09:47:21 +0000

    fix init lua and change lf

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,6 @@
 require('packages')
+require('config')
+require('keybindings')
 
 local configs = require'nvim-treesitter.configs'
 configs.setup {
@@ -58,7 +60,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['sumneko_lua'].setup {
     capabilities = capabilities
