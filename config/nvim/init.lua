1fe057e601d3b79b96761e317b5f37b7e9958e46
DJ Adams <dj.adams@sap.com>
2022-10-19 06:38:16 +0100

    get rid of annoying msg

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -60,8 +60,8 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+--local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['sumneko_lua'].setup {
-    capabilities = capabilities
-}
+--require('lspconfig')['sumneko_lua'].setup {
+--    capabilities = capabilities
+--}
