0da969da58d293074e8538ff65748c1fd1425e65
DJ Adams <dj.adams@sap.com>
2022-12-06 13:41:08 +0000

    mass changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+vim.opt.wrap = true
 
 require('packer').startup(function(use)
   -- Packer manages itself
