7c8d573d80d3c346c0fd34dabdf304cab3f57d73
DJ Adams <dj.adams@sap.com>
2023-11-10 16:03:25 +0000

    reduce nvim config

--- config/nvim/after/plugin/mason.lua
+++ config/nvim/after/plugin/mason.lua
@@ -3,16 +3,16 @@ local lsp_zero = require('lsp-zero')
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
-        "awk_ls",
-        "bashls",
-        "dockerls",
-        "docker_compose_language_service",
-        "jqls",
-        "jsonls",
-        "lua_ls",
-        "marksman",
+        -- "awk_ls",
+        -- "bashls",
+        -- "dockerls",
+        -- "docker_compose_language_service",
+        -- "jqls",
+        -- "jsonls",
+        -- "lua_ls",
+        -- "marksman",
         "tsserver",
-        "yamlls"
+        -- "yamlls"
     },
     handlers = {
         lsp_zero.default_setup
