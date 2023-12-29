4d49704252a7929df9c26a38d8f985c916b826d7
DJ Adams <dj.adams@sap.com>
2023-12-29 11:07:08 +0000

    install nvim part of vim-tmux-navigator

--- config/nvim/lua/qmacro/lazy.lua
+++ config/nvim/lua/qmacro/lazy.lua
@@ -60,6 +60,24 @@ require('lazy').setup({
         'ThePrimeagen/harpoon'
     },
 
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+
     -- Completion
     { 'hrsh7th/cmp-buffer' },
     { 'hrsh7th/cmp-path' },
