1ec78463b03d7d932374c94b98469e383048e334
DJ Adams <qmacro@gmail.com>
2025-03-16 11:13:02 +0000

    clean up path

--- bashrc.d/10-path.sh
+++ bashrc.d/10-path.sh
@@ -1,16 +1,7 @@
-export PATH="/usr/local/opt/openjdk/bin:$PATH" # brew-installed Java
-
-# Brew-installed Gnu utils
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
-
 # Add dotfiles scripts path and subpaths
 while read -r path; do
   export PATH="$PATH:$path"
 done <<< "$(find "$DOTFILES/scripts" -type d)"
 
 # Have my user-specific bin dir take priority
-export PATH="$HOME/bin:$PATH"
-
-# Add krew for kubectl
-export PATH="${KREW_ROOT:-$HOME/.config/krew}/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
