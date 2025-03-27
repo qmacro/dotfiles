3509b34a4b8f22d072d419ee8a2f89b696a4d398
DJ Adams <qmacro@gmail.com>
2025-03-27 07:57:08 +0000

    Add REPL support, incl split right

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.signcolumn = "yes:1"
 vim.opt.cursorline = true
+vim.opt.splitright = true
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 
