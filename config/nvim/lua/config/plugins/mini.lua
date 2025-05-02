7ac6744635cb2a98bcdc6a501eefcb9ab613a3c7
DJ Adams <qmacro@gmail.com>
2025-05-02 15:33:11 +0000

    remove mini starter for now, try mini surround

--- config/nvim/lua/config/plugins/mini.lua
+++ config/nvim/lua/config/plugins/mini.lua
@@ -3,8 +3,7 @@ return {
     'echasnovski/mini.nvim',
     config = function()
       require('mini.statusline').setup({ use_icons = true })
-      require('mini.starter').setup()
-      vim.keymap.set("n", "<space>ms", MiniStarter.open)
+      require('mini.surround').setup()
     end
   }
 }
