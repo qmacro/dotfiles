7ceb5bb0b19cfa7afbc83c82418ea976e5a52b67
DJ Adams <qmacro@gmail.com>
2026-05-06 14:47:08 +0000

    add to PATH
    - put npm global execs first
    - add games for lolcat

--- bashrc.d/10-path.sh
+++ bashrc.d/10-path.sh
@@ -7,4 +7,10 @@ done <<< "$(find "$DOTFILES/scripts" -type d)"
 export PATH="$HOME/.local/bin:$PATH"
 
 # Add globally installed NPM package executables accessible too
-export PATH="$PATH:$HOME/.npm-packages/bin"
+export PATH="$HOME/.npm-packages/bin:$PATH"
+
+# Temp path for krew (grr non standard location)
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# Add games dir for lolcat (!)
+export PATH="$PATH:/usr/bin/games"
