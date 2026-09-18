4a7bdaef88e5e28e9dec91ef26609a0a8913c742
DJ Adams <qmacro@gmail.com>
2026-09-18 15:34:50 +0000

    add LSP features

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,6 +9,28 @@ callback = function()
   }
 end})
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+    if client:supports_method('textDocument/definition') then
+      vim.keymap.set('n', 'grd', vim.lsp.buf.definition)
+    end
+  end
+})
+
 vim.filetype.add({
   extension = {
     ['cds'] = 'cds',
